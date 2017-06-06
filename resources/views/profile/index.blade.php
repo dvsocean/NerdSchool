648ee2b45f037cbf124919118958d3a298d9bae0
dvsocean <dvsocean@icloud.com>
2017-06-05 17:27:09 -0700

    changed error page to includes file

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -340,53 +340,5 @@
 </html>
 
 @else
-    <!DOCTYPE HTML>
-    <html>
-    <head>
-        <title>Uknown user!!</title>
-        @include('includes.header')
-    </head>
-    <body>
-
-    <br><br>
-    <br><br>
-    <br><br>
-
-
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <!--PLACEHOLDER-->
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <img src="images/user_not_found.png" class="img-responsive">
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <!--PLACEHOLDER-->
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <!--PLACEHOLDER-->
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <h1>User account not detected</h1><br>
-                <h6>Please login or create an account</h6>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                <!--PLACEHOLDER-->
-            </div>
-        </div>
-    </div>
-
-
-
-    @include('includes.footer')
-    </body>
-    </html>
+    @include('includes.error')
 @endif
