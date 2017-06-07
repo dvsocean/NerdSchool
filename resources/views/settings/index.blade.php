58e6229cd637e0ddee6ed66aae64030994ea5e95
dvsocean <dvsocean@icloud.com>
2017-06-07 16:53:41 -0700

    settings page validation and styles complete with no errors

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -6,69 +6,29 @@
     <!--HEADER-->
     @include('includes.settings_header')
     <!--HEADER-->
+
+    <!--GOOGLE FONT-->
+    <link href="https://fonts.googleapis.com/css?family=Modak" rel="stylesheet">
+    <!--GOOGLE FONT-->
+
+    <!--SETTINGS PAGE STYLES-->
+    <link rel="stylesheet" href="assets/css/settings_page_styles.css">
+    <!--SETTINGS PAGE STYLES-->
+
+    <style>
+
+    </style>
 </head>
 <body>
 <?php $user= Auth::user(); ?>
 
 <div class="container-fluid custom_container">
-    <h1>SETTINGS</h1>
+    <h1 id="heading_title">SETTINGS</h1>
     <img src="images/gears.png" class="img-responsive" height="100" width="100" id="gears">
 </div>
 
 <br><br>
         <!--CUSTOM SETTINGS PAGE-->
-        <script>
-            $(function(){
-                //Declarations
-                var old_password= $('#old_password');
-                var new_password= $('#new_password');
-                var submitButton= $('#submitButton');
-                var old_password_confirm= $('#old_password_confirm');
-                var old_password_error= $('#old_password_error');
-                var empty_field_error= $('#empty_field_error');
-                var password_match_error= $('#password_match_error');
-
-                //Automatically disable submit button
-                submitButton.prop('disabled', true);
-
-                //First check that the user knows his original password
-                old_password.blur(function(){
-                    var old_password= $('#old_password').val();
-                    var user_id= $('#user_id').val();
-                    $.ajax({
-                        url:"/ajax_verify",
-                        type:"GET",
-                        data:{old_password: old_password, user_id: user_id},
-                        success: function(data){
-                            if(data){
-                                old_password_confirm.html("<div class='alert alert-success text-center'>" + data + "</div>");
-                                old_password_confirm.delay(5000).fadeOut('slow');
-                                submitButton.prop('disabled', false);
-                            } else {
-                                $('#old_password').focus();
-                                old_password_error.html("<div class='alert alert-danger text-center'>NOT YOUR ORIGINAL PASSWORD</div>");
-                                old_password_error.delay(5000).fadeOut('slow');
-                                submitButton.prop('disabled', true);
-
-                            }
-                        }
-                    });
-
-                    $('#passwordForm').submit(function(e){
-                        if($('#new_password').val() == '' || $('#retype_password').val() == ''){
-                            empty_field_error.html("<div class='alert alert-danger text-center'>Fields cannot be left blank</div>");
-                            e.preventDefault();
-                        }
-
-                        if($('#new_password').val() != $('#retype_password').val()){
-                            password_match_error.html("<div class='alert alert-danger text-center'>Your passwords do not match</div>");
-                            e.preventDefault();
-                        }
-                    });
-                });
-            });
-        </script>
-
         <section id="main" class="main">
             <div class="container">
                 <div class="row">
@@ -97,14 +57,6 @@
                         </form><br><br>
                     </div>
 
-                    <script>
-                        $(function(){
-                           $('#notifyEmailForm').change(function(){
-                               $('#notifyEmailForm').submit();
-                           });
-                        });
-                    </script>
-
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                         <h4>Notify my email when someone responds to my thread</h4><br>
                         {!! Form::model($user, ['method'=> 'PATCH', 'action'=>['SettingsController@update', $user->id], 'id'=>'notifyEmailForm']) !!}
@@ -126,49 +78,6 @@
                         <p>This email is used to notify you of thread responses and other
                         changes to your account</p><br>
 
-                        <script>
-                            $(function(){
-
-                                function validateEmail(Email) {
-                                    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-                                    return pattern.test(Email);
-                                }
-
-                               $('#old_email').blur(function(){
-                                   var email= $(this).val();
-                                   $.ajax({
-                                       url:"/verifyEmail",
-                                       method:"GET",
-                                       data: {email:email},
-                                       success: function(data){
-                                           if(data){
-                                               $('#return_message').html("<div class='alert alert-success text-center'>" + data + "</div>");
-                                           } else {
-                                               $('#return_message').html("<div class='alert alert-danger text-center'>EMAIL ADDRESS NOT VALID!</div>");
-                                               $('#return_message').delay(3000).fadeOut('slow');
-                                               $('#old_email').focus();
-                                           }
-                                        }
-                                   });
-                               });
-
-
-                               $('#change_email_form').submit(function(e){
-                                   var originalEmail= $('#old_email');
-                                   var newEmail= $('#new_email');
-                                   if(originalEmail.val() == '' || newEmail.val() == ''){
-                                       $('#return_matching').html("<div class='alert alert-danger text-center'>Fields cannot be blank</div>");
-                                       e.preventDefault();
-                                       $('#return_matching').delay(5000).fadeOut('slow');
-                                   }
-                                   if(!validateEmail(newEmail.val())){
-                                       $('#return_message').html("<div class='alert alert-danger text-center'>Your new email address is the wrong format</div>");
-                                       e.preventDefault();
-                                   }
-                               });
-
-                            });
-                        </script>
                         <div id="return_message"></div>
                         <div id="return_matching"></div>
 
@@ -207,6 +116,10 @@
         </section>
         <!--CUSTOM SETTINGS PAGE-->
 
+
+<!--VALIDATION AND NOTIFY SETTINGS JS-->
+<script src="assets/js/custom_validation.js"></script>
+<!--VALIDATION AND NOTIFY SETTINGS JS-->
 
 @include('includes.settings_footer')
 
